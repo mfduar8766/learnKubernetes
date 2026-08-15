aefa55b8cae1434abb96b1d8ae632f7c56869c91
mfduar8766 <mfduar8766@gmail.com>
2026-08-14 20:59:37 -0400

    1 job

--- scripts/buildDockerImages.sh
+++ scripts/buildDockerImages.sh
@@ -5,6 +5,8 @@ cd ../
 GATE_WAY_IMG_NAME="gateway-app:v1"
 USERS_SERVICE_API_IMG_NAME="users-app:v1"
 MQTT_INIT_CONTAINER="mqtt-init-container-app:v1"
+TAIL_WIND_VRSION=4.2.2
+
 
 echo "📦 Building MQTT Init Container: $MQTT_INIT_CONTAINER..."
 # Point to the specific Dockerfile inside the initContainers folder
@@ -23,4 +25,4 @@ docker build -t $GATE_WAY_IMG_NAME -f app-gateway/Dockerfile .
 
 echo "📦 Building Users Service: $USERS_SERVICE_API_IMG_NAME..."
 # Point to the api/users Dockerfile
-docker build -t $USERS_SERVICE_API_IMG_NAME -f api/users/Dockerfile .
+docker build --build-arg=$TAIL_WIND_VRSION -t $USERS_SERVICE_API_IMG_NAME -f api/users/Dockerfile .
